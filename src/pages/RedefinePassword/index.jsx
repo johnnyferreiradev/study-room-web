e89e31208413069d855f1ec52bebab2c49d1d4ee
johnnyferreiradev <johnnyferreiradevweb@gmail.com>
2021-03-13 13:19:05 -0300

    Creating the password reset page

---
+++ src/pages/RedefinePassword/index.jsx
@@ -0,0 +1,60 @@
+import React from 'react';
+
+import { Container, Row, Column } from 'components/Grid';
+import Banner from 'components/Banner';
+import Card from 'components/Card';
+import { Button, LinkButton } from 'components/Buttons';
+
+import StyledRedefinePassword from './styles';
+
+function RedefinePassword() {
+  return (
+    <StyledRedefinePassword>
+      <Container>
+        <Row>
+          <Column desktop="6" tablet="6" mobile="12" className="flex j-c-center a-i-center banner-column">
+            <Banner>
+              <div className="banner-text">
+                <p className="txt-secondary">Bem-vindo ao</p>
+                <h1 className="title">Study Room</h1>
+                <h3 className="subtitle txt-dark">Plataforma de gerenciamento de conteúdo para escolas</h3>
+              </div>
+            </Banner>
+          </Column>
+          <Column desktop="6" tablet="6" mobile="12" className="flex f-column j-c-center a-i-center form-area">
+            <Card width="344px" className="mb-3">
+              <h2 className="text-center">Criar nova senha</h2>
+              <p className="text-center txt-secondary mb-3">Insira a sua nova senha de acesso</p>
+
+              <div className="form">
+                <div className="form-group mb-2">
+                  <label htmlFor="redefine-password">
+                    <p>Senha</p>
+                    <input type="password" placeholder="" id="redefine-password" />
+                  </label>
+                </div>
+
+                <div className="form-group mb-2">
+                  <label htmlFor="redefine-confirm-password">
+                    <p>Repita a senha</p>
+                    <input type="password" placeholder="" id="redefine-confirm-password" />
+                  </label>
+                </div>
+
+                <div className="form-group mb-3">
+                  <Button theme="primary" fluid>Salvar</Button>
+                </div>
+              </div>
+            </Card>
+
+            <div className="flex">
+              <LinkButton to="/">Voltar para o Login</LinkButton>
+            </div>
+          </Column>
+        </Row>
+      </Container>
+    </StyledRedefinePassword>
+  );
+}
+
+export default RedefinePassword;
--- src/routes/index.js
+++ src/routes/index.js
@@ -5,6 +5,7 @@ import { BrowserRouter, Switch } from 'react-router-dom';
 import Login from 'pages/Login';
 import Register from 'pages/Register';
 import RecoveryPassword from 'pages/RecoveryPassword';
+import RedefinePassword from 'pages/RedefinePassword';
 
 import Public from './public';
 
@@ -14,6 +15,7 @@ const Routes = () => (
       <Public path="/" exact component={Login} />
       <Public path="/register" exact component={Register} />
       <Public path="/recovery-password" exact component={RecoveryPassword} />
+      <Public path="/redefine-password" exact component={RedefinePassword} />
     </Switch>
   </BrowserRouter>
 );
