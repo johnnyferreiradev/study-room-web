bd55c94ee1ba760bccc66203338de44c25cd9638
johnnyferreiradev <johnnyferreiradevweb@gmail.com>
2021-03-13 11:36:29 -0300

    Creating login screen banner

--- src/pages/Login/index.jsx
+++ src/pages/Login/index.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 
 import { Container, Row, Column } from 'components/Grid';
+import Banner from 'components/Banner';
 
 import StyledLogin from './styles';
 
@@ -10,7 +11,13 @@ function Login() {
       <Container>
         <Row>
           <Column desktop="6" tablet="6" mobile="6" className="flex j-c-center a-i-center">
-            Banner side
+            <Banner>
+              <div className="banner-text">
+                <p className="txt-secondary">Bem-vindo ao</p>
+                <h1 className="title">Study Room</h1>
+                <h3 className="subtitle txt-dark">Sistema de gerenciamento de conteúdo para escolas</h3>
+              </div>
+            </Banner>
           </Column>
           <Column desktop="6" tablet="6" mobile="6" className="flex j-c-center a-i-center">
             Login card side
