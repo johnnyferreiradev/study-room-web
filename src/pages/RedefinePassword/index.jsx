d7a99d4016bf2f6caf117e4eca928e6cc9fcdbb2
johnnyferreiradev <johnnyferreiradevweb@gmail.com>
2021-03-15 21:54:07 -0300

    Adding feedback message at login and reset password page

--- src/pages/RedefinePassword/index.jsx
+++ src/pages/RedefinePassword/index.jsx
@@ -1,4 +1,8 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { useDispatch } from 'react-redux';
+import { useHistory } from 'react-router-dom';
+
+import showSnackbar from 'store/actions/snackbar/showSnackbar';
 
 import { Container, Row, Column } from 'components/Grid';
 import Banner from 'components/Banner';
@@ -8,6 +12,44 @@ import { Button, LinkButton } from 'components/Buttons';
 import StyledRedefinePassword from './styles';
 
 function RedefinePassword() {
+  const dispatch = useDispatch();
+  const history = useHistory();
+
+  const [redefineState, setRedefineState] = useState({
+    password: '',
+    confirmPassword: '',
+  });
+
+  const handleRedefineField = ({ target }) => {
+    switch (target.name) {
+      case 'password':
+        setRedefineState((prevState) => ({ ...prevState, password: target.value }));
+        break;
+      case 'confirmPassword':
+        setRedefineState((prevState) => ({ ...prevState, confirmPassword: target.value }));
+        break;
+      default:
+        throw new Error('invalid option');
+    }
+  };
+
+  const handleRedefine = () => {
+    const { password, confirmPassword } = redefineState;
+
+    if (!password || !confirmPassword) {
+      dispatch(showSnackbar('Você precisa preencher todos os campos', 'danger'));
+      return;
+    }
+
+    if (password !== confirmPassword) {
+      dispatch(showSnackbar('As senhas não coincidem', 'danger'));
+      return;
+    }
+
+    dispatch(showSnackbar('Senhas redefinidas com sucesso!', 'success'));
+    history.push('/');
+  };
+
   return (
     <StyledRedefinePassword>
       <Container>
@@ -24,19 +66,33 @@ function RedefinePassword() {
                 <div className="form-group mb-2">
                   <label htmlFor="redefine-password">
                     <p>Senha</p>
-                    <input type="password" placeholder="" id="redefine-password" />
+                    <input
+                      type="password"
+                      name="password"
+                      value={redefineState.password}
+                      placeholder=""
+                      id="redefine-password"
+                      onChange={handleRedefineField}
+                    />
                   </label>
                 </div>
 
                 <div className="form-group mb-2">
                   <label htmlFor="redefine-confirm-password">
                     <p>Repita a senha</p>
-                    <input type="password" placeholder="" id="redefine-confirm-password" />
+                    <input
+                      type="password"
+                      name="confirmPassword"
+                      value={redefineState.confirmPassword}
+                      placeholder=""
+                      id="redefine-confirm-password"
+                      onChange={handleRedefineField}
+                    />
                   </label>
                 </div>
 
                 <div className="form-group mb-3">
-                  <Button theme="primary" fluid>Salvar</Button>
+                  <Button theme="primary" fluid onClick={handleRedefine}>Salvar</Button>
                 </div>
               </div>
             </Card>
